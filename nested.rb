1c2a8f1f76567d7580b10d17b4491171913aa04f
Marco Frisina <marco@frisina.xyz>
2019-06-25 22:50:27 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
 					:languages => ["C"]
 				}
 		 }
-	
+
 	programmer_hash[:dennis_ritchie][:languages]
 end
 
@@ -76,7 +76,7 @@ def adding_matz
 					:languages => ["C"]
 				}
 		 }
-	
+
 	programmer_hash[:yukihiro_matsumoto][:known_for] = "ruby"
 	programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
 end
