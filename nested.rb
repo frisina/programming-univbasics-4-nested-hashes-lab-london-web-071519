7fc95606e6510cff561d42bc14dad21a809b76c2
Marco Frisina <marco@frisina.xyz>
2019-06-25 22:46:10 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -38,22 +38,7 @@ end
 #    :languages => ["LISP", "C"]
 # }
 
-	programmer_hash =
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
-
+	
 
 end
 
