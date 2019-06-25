fc59c1499d42f834fda63162f7643b939e47940b
Marco Frisina <marco@frisina.xyz>
2019-06-25 22:37:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -22,21 +22,6 @@ end
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	programmer_hash[:alan_kay][:known_for]
-	programmer_hash =
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
 end
 
 def dennis_ritchies_language
